49e9c22504c32d21ef5bf13523db3af1efe109bd
Albin Larsson Forsberg <albinfor@kth.se>
2021-02-25 11:38:43 +0100

    Best controller brah

--- Assignment 3/HW3_source/CFiles/OwnCode/Controller.c
+++ Assignment 3/HW3_source/CFiles/OwnCode/Controller.c
@@ -8,49 +8,45 @@ if (state == 0){
     //Keep close to start point and get correct angle
     v = K_omega*(cos(theta*PI/180)*(x0-x)+sin(theta*PI/180)*(y0-y));
     w = K_Psi*theta_error;
-    if (abs(theta_error)<1) state = 1;
+    if (abs(theta_error)<4) state = 1;
 } else if (state == 1){
     Serial.print("Driving");
     //Keep close to line and drive to goal
     v = K_omega*(cos(theta_g*PI/180)*(xg-x)+sin(theta_g*PI/180)*(yg-y));
     w = K_Psi*(sin(theta_g*PI/180)*(x-x0+p*cos(theta*PI/180))-cos(theta_g*PI/180)*(y-y0+p*sin(theta*PI/180)));
-    if (abs(x-xg)<3 && abs(y-yg)<3) state = 2;
+    if (abs(x-xg)<15 && abs(y-yg)<15) state = 2;
 } else if (state == 2){
     Serial.print("Stopping");
     v = 0;
     w = 0;
+    send_done();
 }
+
+
+//In case students implement limits on control outputs
+limit_v = 400;
+limit_w = 200;
+if (v >= limit_v){
+        v=limit_v;
+}
+if (v <= -limit_v){
+        v=-limit_v;
+}
+if ( w >= limit_w){
+        w=limit_w;
+}
+if ( w <= -limit_w){
+        w=-limit_w;
+}
+
 Serial.print("\nv = ");
 Serial.print(v);
 Serial.print("\nw = ");
 Serial.print(w);
 
-
-
-
-/*
-if (abs(theta_error) > 1 && abs(pos_error)>0.16){
-    //GOTO start point
-    w = K_Psi*theta_error;
-    v = 0;
-    //Translational fixing
-    double d_g;
-    d_g = cos(theta*PI/180)*(x0-x)+sin(theta*PI/180)*(y0-y);
-    v = K_omega*d_g;
-    //w = 0;
-}else{
-    //Rotate towards goal position
-    theta_R = atan2(yg-y,xg-x)*180/PI;
-    theta_error = theta_R-theta;
-    v = 0; 
-    w = K_Psi*theta_error;
-}
-*/
-
-
-
-
 //Convert v and w to control signal
 right = v+w/2;
 left = v-w/2;
+
+
 
--- Assignment 3/HW3_source/CFiles/OwnCode/OwnVariables.c
+++ Assignment 3/HW3_source/CFiles/OwnCode/OwnVariables.c
@@ -5,7 +5,17 @@ double theta_error;
 double theta_g;
 int state = 0;
 
+//Simulator parameters
+//double h = 0.02; //in seconds
+double h = 1; //in seconds
+int limit_v;
+int limit_w;
+
+
 //Controller parameters
-double K_Psi = 4;
-double K_omega = 4;
-double p = 4;
+//double K_Psi = 0.01*L_true/(h*R_true); //Ain't working brah
+//double K_omega = 0.0001*1/(h*R_true); //Ain't working brah
+
+double p = 40;
+double K_Psi = 5;
+double K_omega = 8;

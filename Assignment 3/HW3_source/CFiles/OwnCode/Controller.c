8ce74f9fc4efc7d11719305f4062495a11f78c89
johnliu98 <68383339+johnliu98@users.noreply.github.com>
2021-02-25 19:10:20 +0100

    Creating two separate K_Psi
    One for rotation, one for driving.

--- Assignment 3/HW3_source/CFiles/OwnCode/Controller.c
+++ Assignment 3/HW3_source/CFiles/OwnCode/Controller.c
@@ -10,19 +10,23 @@ if (abs(theta_R-theta) < abs(theta_R-theta-360)){
 
 if (state == 0){
     Serial.print("Rotating");
+
     //Keep close to start point and get correct angle
     d0 = (cos(theta*PI/180)*(x0-x)+sin(theta*PI/180)*(y0-y));
     v = K_omega*d0;
-    w = K_Psi*theta_error;
+
+    w = K_Psi_rotation*theta_error;
+
     if (abs(theta_error)<2) state = 1;
 } else if (state == 1){
     Serial.print("Driving");
+
     //Keep close to line and drive to goal
     dg = (cos(theta_g*PI/180)*(xg-x)+sin(theta_g*PI/180)*(yg-y));
     v = K_omega*dg;
 
     dp = (sin(theta_g*PI/180)*(x-x0+p*cos(theta*PI/180))-cos(theta_g*PI/180)*(y-y0+p*sin(theta*PI/180)));
-    w = K_Psi*dp;
+    w = K_Psi_drive*dp;
 
     if (abs(x-xg)<5 && abs(y-yg)<5) state = 2;
 } else if (state == 2){
@@ -49,10 +53,10 @@ if ( w <= -limit_w){
         w=-limit_w;
 }
 
-Serial.print("\nv = ");
-Serial.print(v);
-Serial.print("\nw = ");
-Serial.print(w);
+//Serial.print("\nv = ");
+//Serial.print(v);
+//Serial.print("\nw = ");
+//Serial.print(w);
 
 //Convert v and w to control signal
 right = v+w/2;
--- Assignment 3/HW3_source/CFiles/OwnCode/OwnVariables.c
+++ Assignment 3/HW3_source/CFiles/OwnCode/OwnVariables.c
@@ -9,16 +9,18 @@ double dp;
 int state = 0;
 
 //Simulator parameters
-//double h = 0.02; //in seconds
-double h = 1; //in seconds
+double R = 0.1001405119340;
+double L = 0.5052864456892;
+double h = 1;
 int limit_v;
 int limit_w;
 
 
 //Controller parameters
-//double K_Psi = L_true/(h*R_true); //Ain't working brah
-//double K_omega = 1/(h*R_true); //still ain't working brah
-
 double p = 50;
-double K_Psi = 5;
-double K_omega = 8;
+//double K_Psi = 5;
+//double K_omega = 10;
+
+double K_Psi_rotation = L/(h*R); //Ain't working brah
+double K_Psi_drive = 100*L/(p*h*R);
+double K_omega = 1/(h*R); //still ain't working brah
